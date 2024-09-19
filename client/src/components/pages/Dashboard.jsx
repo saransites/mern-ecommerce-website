5982f26e4c23d4fc49ed6ef8983cbe0be52f8153
saran <sarannatarajan7@gmail.com>
2024-09-19 15:34:42 +0530

    again fix

--- client/src/components/pages/Dashboard.jsx
+++ client/src/components/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useTheme } from "@mui/material";
 import React, { useState } from "react";
-import { FaStar } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { Star } from '@mui/icons-material'
 
@@ -165,7 +164,7 @@ const HomePage = () => {
                 <p className="text-gray-600">${product.price.toFixed(2)}</p>
                 <div className="flex items-center mt-2">
                   {[...Array(5)].map((_, i) => (
-                    <FaStar
+                    <Star
                       key={i}
                       className={`h-5 w-5 ${
                         i < Math.floor(product.rating)
